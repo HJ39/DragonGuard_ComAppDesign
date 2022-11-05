244a1cf97e507d4a2cd9f4ece74444895afe64fc
JJ <jeonghojin03@gmail.com>
2022-11-05 17:31:32 +0900

    Main, Second, Third Storyboard divide files

--- Monttak_DragonGuard/MainViewController.swift
+++ Monttak_DragonGuard/MainViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 // Main화면 ViewController
 class MainViewController: UIViewController {
 
+    @IBOutlet var first: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func firstbutton(_ sender: UIButton) {
+        let secondStory = UIStoryboard(name: "Second", bundle: nil)
+        let secondScreen = secondStory.instantiateViewController(withIdentifier: "SecondScreen") as! SecondViewController
+        
+        self.navigationController?.pushViewController(secondScreen, animated: true)
+//        self.present(secondScreen, animated: true)
+    }
+    
 }
 
--- Monttak_DragonGuard/SecondViewController.swift
+++ Monttak_DragonGuard/SecondViewController.swift
@@ -8,9 +8,21 @@
 import UIKit
 
 //Second화면 ViewController
-class SecondViewController:UIViewController{
+class SecondViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    //버튼 클릭 시 세 번째 화면으로 넘어가는 메소드
+    @IBAction func Click_Go_Third(_ sender: Any) {
+        let third = UIStoryboard(name: "Third", bundle: nil)
+        
+        // UIViewController에서 ThirdViewController로 다운 캐스팅
+        let thirdScreen = third.instantiateViewController(withIdentifier: "ThirdScreen") as! ThirdViewController
+        
+        self.navigationController?.pushViewController(thirdScreen, animated: true) //네비게이션 Controller 전용 넘어가는 구문
+    }
+    
 }
