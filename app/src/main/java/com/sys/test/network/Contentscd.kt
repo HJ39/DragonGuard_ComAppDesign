94b554bf9bfdf8192f3f805825e42ab32a362459
posite <su84053@gmail.com>
2022-11-13 22:08:07 +0900

    폴더화

--- app/src/main/java/com/sys/test/network/Contentscd.kt
+++
@@ -1,9 +0,0 @@
-package com.sys.test.network
-
-import java.io.Serializable
-
-data class Contentscd(
-    val label: String,
-    val refId: String,
-    val value: String
-):Serializable
--- app/src/main/java/com/sys/test/network/Photoid.kt
+++
@@ -1,9 +0,0 @@
-package com.sys.test.network
-
-import java.io.Serializable
-
-data class Photoid(
-    val imgpath: String,
-    val photoid: Long,
-    val thumbnailpath: String
-):Serializable
