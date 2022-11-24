b55121404cb96e067a80103f382bb646f5359902
sammuelwoojae <seoullian@gmail.com>
2022-11-25 03:33:55 +0900

    메인화면 버튼, 텍스트배경, 텍스트타이틀 삽입
    비율 맞춰주기 필요

--- iOS/Monttak_DragonGuard/MainViewController.swift
+++ iOS/Monttak_DragonGuard/MainViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 // Main화면 ViewController
 class MainViewController: UIViewController {
+    @IBOutlet var textview: UITextView!
+    @IBOutlet var texttitle: UILabel!
+    @IBOutlet var stackview: UIStackView!
     let screenWidth = UIScreen.main.bounds.size.width // 뷰 전체 폭 길이
     let screenHeight = UIScreen.main.bounds.size.height // 뷰 전체 높이 길이
     
@@ -18,18 +21,25 @@ class MainViewController: UIViewController {
         self.navigationItem.backButtonTitle = " "    //SecondScreen 뒤로가기 버튼 텍스트 없앰
         
         //백그라운드를 이미지로 설정
-        guard let img = UIImage(named: "배경2") else{ return }
+        guard let img = UIImage(named: "secondbackground") else{ return }
         //이미지크기를 조절해서 백그라운드에 적용
         self.view.backgroundColor = UIColor(patternImage: img.resize(newWidth: screenWidth,newHeight: screenHeight) )
         
+        //백그라운드 텍스트 타이틀 설정(제목)
+        guard let img2 = UIImage(named: "texttitle") else{ return }
+        //백그라운드에 적용
+        texttitle.backgroundColor = UIColor(patternImage: img2)
         
+        //백그라운드를 이미지로 설정
+        guard let img1 = UIImage(named: "textviewimg") else{ return }
+        //백그라운드에 적용
+        stackview.backgroundColor = UIColor(patternImage: img1)
     }
 
     
     @IBAction func Click_Go_SecondScreen(_ sender: UIButton) {
         let secondStory = UIStoryboard(name: "Second", bundle: nil)
         let secondScreen = secondStory.instantiateViewController(withIdentifier: "SecondScreen") as! SecondViewController
-        secondScreen.choiceButton = "볼거리"   //사용자가 누르는 버튼 이름을 넣어야함
         self.navigationController?.pushViewController(secondScreen, animated: true)
     }
     
